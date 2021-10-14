abf9fa205f8166d725500a58fecae97a9487c1b5
othman <othmanfroukh@gmail.com>
2021-10-15 00:27:50 +0100

    x

--- src/Footer/footer.js
+++ src/Footer/footer.js
@@ -3,7 +3,7 @@ import classes from './footer.module.css'
 const Footer = () => {
     return ( 
         <div className={classes.footer}>
-            <p style={{textAlign:"center"}}>© Copyright 2021. All Rights reserved by FROUKH Othman</p>
+            <p style={{textAlign:"center",fontSize:"10px"}}>© Copyright 2021. All Rights reserved by FROUKH Othman</p>
         </div>
      );
 }
