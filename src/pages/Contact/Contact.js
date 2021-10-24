f87406c9ae9cfa7b63c5586f6e35ebb745948e55
othman <othmanfroukh@gmail.com>
2021-10-25 00:05:17 +0200

    adding EmailJs

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,21 +1,44 @@
 import shake from '../../assets/shake.svg'
 import classes from "./Contact.module.css"
-const Contact = () => {
+import emailjs from 'emailjs-com';
+import { useRef, useState } from 'react';
+const Contact = (props) => {
+    const form = useRef();
+    console.log(props)
+    const [sendOk, setSendOk] = useState(false);
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('service_m6a3l2b', 'template_n1m1i97', form.current, 'user_0bb77VuYIwqEOzOmW7zWW')
+            .then((result) => {
+                console.log("result", result)
+                setSendOk(true)
+            }, (error) => {
+                setSendOk(false);
+            });
+        setTimeout(() => {
+            setSendOk(false);
+        }, 3000)
+        e.target.reset()
+    };
     return (
-        <div className={"row justify-content-center align-items-center m-0 "+classes.row_contact}>
-            <div className={"col-md-6 col-sm-12 "+classes.left_contact}>
+        <div className={"row justify-content-center align-items-center m-0 " + classes.row_contact}>
+            <div className={"col-md-6 col-sm-12 " + classes.left_contact}>
                 <img src={shake} alt="" className={classes.img} />
             </div>
-            <div className={"col-md-6 col-sm-12 "+classes.right_contact}>
+            <div className={"col-md-6 col-sm-12 " + classes.right_contact}>
                 <h2>Contact Me.</h2>
-                <form action="">
-                    <input type="email" name="email" id="email" placeholder="contact@othmanfroukh.com"/>
+                <form ref={form} onSubmit={sendEmail}>
+                    <input type="text" name="name" id="name" placeholder="Othman FROUKH" />
+                    <input type="email" name="email" id="email" placeholder="contact@othmanfroukh.com" />
+                    <input type="text" name="Subject" id="Subject" placeholder="Subject" />
                     <textarea name="message" id="message" cols="30" rows="10" placeholder="write your message here">
 
                     </textarea>
-                    <button> Send </button>
+                    <button type="submit"> Send </button>
                 </form>
+                {sendOk && <span style={{ color: "green" }}>Thank you for your message, I'll answer you ASAP 😃.</span>}
             </div>
+
         </div>
     );
 }
